c7cba997ad5e91b962b50ae6f9b741d9e90a9926
GianKey247 <144612814+GianKey247@users.noreply.github.com>
2025-01-02 21:22:07 -0800

    Add files via upload

---
+++ front_end/javascript/slideshow_2_slide_test.js
@@ -0,0 +1,22 @@
+let slideIndex = [1,1];
+let slideId = ["pictures_Games", "video_Features"]
+
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+    let i;
+    let x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) {slideIndex[no] = 1}
+    if (n < 1) {slideIndex[no] = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex[no]-1].style.display = "block";
+}
+
+window.onload = function () {
+    showSlides(1, 0);
+    showSlides(1, 1);
+}
